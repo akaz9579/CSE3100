3051e2db37bf52e2b743cd30d999279d02665140
akaz9579 <akaz9579@gmail.com>
2025-09-15 18:19:30 -0400

    lab work

---
+++ point.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int x = 21;
+    int *P = &x;
+
+    printf("x: %d\n",x);
+    printf("&x: %p\n",&x);
+    printf("*P: %d\n",*P);
+    printf("P point %p\n",P);
+    printf("&*P  %p\n",&*P);
+    x = 23;
+    printf("x: %d\n",x);
+    printf("&x: %p\n",&x);
+    printf("*P: %d\n",*P);
+    printf("P point: %p\n",P);
+    printf("&*P %p\n",&*P);
+
+
+
+    return 0;
+}
